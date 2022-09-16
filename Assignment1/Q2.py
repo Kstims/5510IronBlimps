89127645eaa637ec0d849d9765198c9d2d6fb1d3
Kyle Stimson <59680457+Kstims@users.noreply.github.com>
2022-09-16 12:50:54 -0600

    Add files via upload

---
+++ Assignment1/Q2.py
@@ -0,0 +1,92 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('_mpl-gallery')
+
+
+
+
+
+
+
+def kinematics(l, r, W, x, y, theta, DT):
+    xNew = x - .5 * (l + r) * np.sin(theta) * DT
+    yNew = y + .5 * (l + r) * np.cos(theta) * DT
+    thetaNew = theta + (1 / W) * (r - l) * DT
+    aVel = (thetaNew - theta) / DT
+    xVel, yVel = getVelocity(x, y, xNew, yNew, DT)
+    return xNew, yNew, thetaNew, aVel, xVel, yVel
+
+
+def getVelocity(x0, y0, x1, y1, DT):
+    distance = np.sqrt(np.square(x1 - x0) + np.square(y1 - y0))
+    velocity = distance / DT
+    magX = x1-x0
+    magY = y1-y0
+    if magX == 0:
+        if magY > 0:
+            thetaDegrees = 90
+        else:
+            thetaDegrees = 180
+    else:
+        thetaDegrees = np.arctan(magY / magX)
+    yVel = velocity * np.sin(thetaDegrees)
+    xVel = velocity * np.cos(thetaDegrees)
+    if np.isnan(yVel):
+        yVel = 0
+    if np.isnan(xVel):
+        xVel = 0
+    print("")
+    return xVel, yVel
+
+
+WIDTH = .3
+DT = .1
+time = 0
+xNot = .15
+yNot = 0
+theta = 0
+xCoord = [xNot]
+yCoord = [yNot]
+xPoint = [xNot]
+yPoint = [yNot]
+
+# (1, .23562, -.23562) = 90 degree clockwise turn
+# (1, -.23562, .23562) = 90 degree counterclockwise turn
+kList = [(5, 1, 1), #movement sequence
+         (1, .23562, -.23562), (1,.3,.3), (1, .23562, -.23562), #turning south sequence
+         (5, 1, 1), #movement sequence
+         (1, -.23562, .23562), (1, .3, .3), (1, -.23562, .23562)] #turning north sequence
+
+
+
+print("(" + str(xNot) + ", " + str(yNot) + ") theta= " + str(theta))
+print("At T = " + str(time))
+for z in range(0, 9):
+    for x in range(0, len(kList)):
+        for y in range(0, kList[x][0] * 10):
+            xNot, yNot, theta, angVel, xV, yV = kinematics(kList[x][1], kList[x][2], WIDTH, xNot, yNot, theta, DT)
+            xCoord.append(xNot)
+            yCoord.append(yNot)
+            time += .1
+            print("Position: (" + str(xNot) + ", " + str(yNot) + ") theta = " + str(theta))
+            print("Trajectory: (" + str(xV) + ", " + str(yV) + ") Angular Velocity = " + str(angVel))
+            print("At T = " + str(time))
+        xPoint.append(xNot)
+        yPoint.append(yNot)
+
+
+
+
+
+
+
+
+
+fig, ax = plt.subplots(constrained_layout=True, figsize = (6, 6))
+ax.step(xCoord, yCoord)
+ax.set_xlabel('x (meters)')
+ax.set_ylabel('y (meters)')
+plt.title("Question 2: Skid Steer coverage of 5x5 plot")
+plt.plot(xPoint, yPoint, linewidth = 2, marker = '.')
+plt.show()

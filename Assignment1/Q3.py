32a73f6968fcbb9a64ff88628dd89b48a6808cf5
Trevor Jex <jextrevor@gmail.com>
2022-09-22 14:38:03 -0600

    Display velocities for Q3

--- Assignment1/Q3.py
+++ Assignment1/Q3.py
@@ -9,50 +9,14 @@ plt.style.use('_mpl-gallery')
 
 
 
-def kinematics(l, r, W, x, y, theta, DT):
-    xNew = x - .5 * (l + r) * np.sin(theta) * DT
-    yNew = y + .5 * (l + r) * np.cos(theta) * DT
-    thetaNew = theta + (1 / W) * (r - l) * DT
-    aVel = (thetaNew - theta) / DT
-    xVel, yVel = getVelocity(x, y, xNew, yNew, DT)
+def kinematics(v, heading, x, y, theta, DT):
+    xNew = x - v * np.sin(heading) * DT
+    yNew = y + v * np.cos(heading) * DT
+    thetaNew = theta
+    aVel = 0
+    xVel = -v * np.sin(heading)
+    yVel = v * np.cos(heading)
     return xNew, yNew, thetaNew, aVel, xVel, yVel
-
-def strafe(l, r, x, y, theta, DT):
-
-    #strafe right
-    if l < r:
-        velocity = abs(r) * DT
-
-    # strafe left
-    else:
-        velocity = l * dt
-
-    yNew = velocity * np.sin(theta) + y
-    xNew =  velocity * np.cos(theta) + x
-    xVel, yVel = getVelocity(x, y, xNew, yNew, DT)
-    return xNew, yNew, xVel, yVel
-
-def getVelocity(x0, y0, x1, y1, DT):
-    distance = np.sqrt(np.square(x1 - x0) + np.square(y1 - y0))
-    velocity = distance / DT
-    magX = x1-x0
-    magY = y1-y0
-    if magX == 0:
-        if magY > 0:
-            thetaDegrees = 90
-        else:
-            thetaDegrees = 180
-    else:
-        thetaDegrees = np.arctan(magY / magX)
-    yVel = velocity * np.sin(thetaDegrees)
-    xVel = velocity * np.cos(thetaDegrees)
-    if np.isnan(yVel):
-        yVel = 0
-    if np.isnan(xVel):
-        xVel = 0
-    print("")
-    return xVel, yVel
-
 
 WIDTH = .3
 DT = .1
@@ -65,12 +29,17 @@ yCoord = [yNot]
 xPoint = [xNot]
 yPoint = [yNot]
 
+xVels = []
+yVels = []
+angVels = []
+times = []
+
 # (1, .23562, -.23562) = 90 degree clockwise turn
 # (1, -.23562, .23562) = 90 degree counterclockwise turn
-kList = [(5, 1, 1, 0), #movement forward sequence
-         (1, -.3, .3, 1), #strafe right sequence
-         (5, -1, -1, 0), #movement backward sequence
-         (1, -.3, .3, 1)] #strafe right sequence
+kList = [(5, 1, 0), #movement forward sequence
+         (1, .3, -np.pi/2), #move right sequence
+         (5, 1, np.pi), #move backward sequence
+         (1, .3, -np.pi/2)] #move right sequence
 
 
 
@@ -80,13 +49,13 @@ print("At T = " + str(time))
 for z in range(0, 9):
     for x in range(0, len(kList)):
         for y in range(0, kList[x][0] * 10):
-            if(kList[x][3] == 0):
-                xNot, yNot, theta, angVel, xV, yV = kinematics(kList[x][1], kList[x][2], WIDTH, xNot, yNot, theta, DT)
-            else:
-                xNot, yNot, xV, yV = strafe(kList[x][1], kList[x][2], xNot, yNot, theta, DT)
-                angVel = 0
+            xNot, yNot, theta, angVel, xV, yV = kinematics(kList[x][1], kList[x][2], xNot, yNot, theta, DT)
             xCoord.append(xNot)
             yCoord.append(yNot)
+            xVels.append(xV)
+            yVels.append(yV)
+            angVels.append(angVel)
+            times.append(time)
             time += .1
             print("Position: (" + str(xNot) + ", " + str(yNot) + ")")
             print("Trajectory: (" + str(xV) + ", " + str(yV) + ") Angular Velocity = " + str(angVel))
@@ -101,10 +70,26 @@ for z in range(0, 9):
 
 
 
-fig, ax = plt.subplots(constrained_layout=True, figsize = (6, 6))
-ax.step(xCoord, yCoord)
-ax.set_xlabel('x (meters)')
-ax.set_ylabel('y (meters)')
-plt.title("Question 3: Swedish wheel coverage of 5x5 plot")
-plt.plot(xPoint, yPoint, linewidth = 2, marker = '.')
+fig, ax = plt.subplots(2,2, constrained_layout=True, figsize=(6,6))
+ax[0, 0].step(xCoord, yCoord)
+ax[0, 0].set_xlabel('x (meters)')
+ax[0, 0].set_ylabel('y (meters)')
+ax[0, 0].plot(xPoint, yPoint, linewidth = 2, marker = '.')
+ax[0, 0].set_title("Swedish Wheel coverage of 5x5 plot")
+
+ax[0, 1].set_xlabel('t (seconds)')
+ax[0, 1].set_ylabel('x velocity (m/s)')
+ax[0, 1].plot(times, xVels)
+ax[0, 1].set_title("X Velocity over time")
+
+ax[1, 0].set_xlabel('t (seconds)')
+ax[1, 0].set_ylabel('y velocity (m/s)')
+ax[1, 0].plot(times, yVels)
+ax[1, 0].set_title("Y Velocity over time")
+
+ax[1, 1].set_xlabel('t (seconds)')
+ax[1, 1].set_ylabel('theta velocity (m/s)')
+ax[1, 1].plot(times, angVels)
+ax[1, 1].set_title("Angular Velocity over time")
+
 plt.show()

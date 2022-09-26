086c2cd0f1f2e1805bf11cbe537f681ab9674aea
Kstims <stimsonkyle@hotmail.com>
2022-09-26 15:11:58 -0600

    Refactored code to allow 3 figures to show and more easily change delta T in charts.

--- Midterm/Q1c.py
+++ Midterm/Q1c.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 plt.style.use('_mpl-gallery')
 
-#Creates the list of commands for the bot to follow every tick
+
+# Creates the list of commands for the bot to follow every tick
 def generateCommands(interval, turn):
     commandList = []
 
@@ -12,7 +14,8 @@ def generateCommands(interval, turn):
 
     return commandList
 
-#Runs the list of commands to move the bot
+
+# Runs the list of commands to move the bot
 def runCommands(kList):
     time = 0
     xNot = -2.5
@@ -44,8 +47,9 @@ def runCommands(kList):
 
     plotCharts(xCoord, yCoord, xPoint, yPoint, xVels, yVels, angVels, times, kList[0][0])
 
-#calculates position and velocities
-def kinematics(x, y, theta, alpha,tInterval):
+
+# calculates position and velocities
+def kinematics(x, y, theta, alpha, tInterval):
     LENGTH = .75
     VELOCITY = 8
 
@@ -53,11 +57,12 @@ def kinematics(x, y, theta, alpha,tInterval):
     yNew = y + VELOCITY * np.cos(theta) * tInterval
     thetaNew = theta + (VELOCITY / LENGTH) * np.tan(alpha) * tInterval
     aVel = (VELOCITY / LENGTH) * np.tan(alpha)
-    xVel = -1*VELOCITY * np.sin(theta)
+    xVel = -1 * VELOCITY * np.sin(theta)
     yVel = VELOCITY * np.cos(theta)
     return xNew, yNew, thetaNew, aVel, xVel, yVel
 
-#draws the figures
+
+# draws the figures
 def plotCharts(xCoordinate, yCoordinate, xPos, yPos, xVelocity, yVelocity, aVelocity, tList, deltaT):
     figure, ax = plt.subplots(2, 2, tight_layout=True, figsize=(6, 6))
     Drawing_uncolored_circle = plt.Circle((0, 0),
@@ -71,7 +76,7 @@ def plotCharts(xCoordinate, yCoordinate, xPos, yPos, xVelocity, yVelocity, aVelo
         ax[0, 0].set(xlim=(-3, 3), ylim=(-3, 3))
 
     ax[0, 0].plot(xPos, yPos, linewidth=2, marker='.')
-    ax[0, 0].set_title("Question 1c: " + 'Delta: \u0394' + "t= "+ str(deltaT))
+    ax[0, 0].set_title("Question 1c: " + 'Delta: \u0394' + "t= " + str(deltaT))
 
     ax[0, 1].set_xlabel('t (seconds)')
     ax[0, 1].set_ylabel('x velocity (m/s)')
@@ -90,12 +95,12 @@ def plotCharts(xCoordinate, yCoordinate, xPos, yPos, xVelocity, yVelocity, aVelo
 
     plt.show(block=False)
 
-#runs the program using different delta-Ts
+
+# runs the program using different delta-Ts
 dt = [.1, .01, 1]
 
-#main driver
+# main driver
 for x in range(len(dt)):
     commandListing = generateCommands(dt[x], -.291457)
     runCommands(commandListing)
 plt.show()
-

fe1310228b47833d77dadd00673d81020d2c52a5
Kstims <stimsonkyle@hotmail.com>
2022-10-05 13:09:43 -0600

    Slightly updated code. Does not run as expected

--- Midterm/4b.py
+++ Midterm/4b.py
@@ -31,19 +31,32 @@ tau = 0.01
 theta = math.pi / 24
 thetaVelocity = 0
 xVelocity = 0
+#distance from the center
+x = 0
+t = 0
 
 #main driver
 
-print("Starting position theta = " + str(theta) + ", " + str(radToDegrees(theta)) + " degrees")
+print("Starting position theta = " + str(theta) + ", " + str(radToDegrees(theta)) + " degrees" + " Pos = " + str(x))
 
 while True:
-    thetaAccNew = theta_acc(length, force, massPole, massCart, theta, thetaVelocity)
-    xAccNew = x_acc(length, force, massPole, massCart, theta, thetaVelocity, thetaAccNew)
+    #thetaAccNew = theta_acc(length, force, massPole, massCart, theta, thetaVelocity)
+    #xAccNew = x_acc(length, force, massPole, massCart, theta, thetaVelocity, thetaAccNew)
+    if (force != 0 ):
+        input()
+    tmp = (
+                  force + length * thetaVelocity ** 2 * math.sin(theta)
+          ) / (massCart + massPole)
+    thetaAccNew = (9.8 * math.sin(theta) - math.cos(theta) * tmp) / (
+            length * (4.0 / 3.0 - massPole * math.cos(theta) ** 2 / (massCart + massPole))
+        )
+    xAccNew = tmp - length * thetaAccNew * math.cos(theta) / (massCart + massPole)
 
     thetaVelocity += thetaAccNew * tau
     thetaOld = theta
     theta += thetaVelocity * tau
     xVelocity += xAccNew
+    x += xVelocity * tau
 
     #determining if pole is to the left (-theta) or right (+theta)
     #pole to the left
@@ -52,27 +65,39 @@ while True:
         if thetaOld >= 0:
             #reversing force and decreasing by half
             force = -1 * abs(force/2)
-            #print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
+            if force == 0:
+                force = -2
+            print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
         else:
-            #increasing force by 50%
-            force = -1 * abs(force*1.5)
-            #print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
+            #increasing force by 1%
+            # force = -1 * abs(force*1.01)
+            # if force == 0:
+            #     force = -2
+            # if force > 10:
+            #     force = 10
+            print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
     #pole to the right
     elif theta > 0:
         if thetaOld <= 0:
             force = abs(force/2)
-            #print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
+            if force == 0:
+                force = 2
+            print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
         else:
-            force = abs(force*1.5)
-            #rint("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
+            # force = abs(force*1.01)
+            # if force == 0:
+            #     force = 2
+            # if force > 10:
+            #     force = 10
+            print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
     #pole is in perfect center. Stopping cart
     else:
         force = 0
-        #print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
-
+        print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
+    t += tau
     #Failure to balance if the pole has dropped below horizon (theta > 90 or theta < -90
     if theta < -90 or theta > 90:
-        print("Failure to balance")
-        print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees")
+        print("Failure to balance at T = " + str(t))
+        print("Theta: " + str(theta) + ", " + str(radToDegrees(theta)) + "degrees. Force: " + str(force) + " Pos = " + str(x))
         break
 

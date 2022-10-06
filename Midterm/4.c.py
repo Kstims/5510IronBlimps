f4da6456b30426c69a7138fb00a2e0ae90f0274f
Kyle Stimson <stimsonkyle@hotmail.com>
2022-10-06 02:10:45 -0600

    Completed 4C

--- Midterm/4.c.py
+++ Midterm/4.c.py
@@ -1,59 +1,70 @@
 import math
 
 
-def theta_acc(length, force, mass_pole, mass_cart, theta, theta_dot):
-    top = 9.8*math.sin(theta) + math.cos(theta)*((-1*force - mass_pole*length*(theta_dot**2)*math.sin(theta)) / (mass_cart + mass_pole))
-    bottom = length*((4/3) - ((mass_pole*(math.cos(theta)**2)) / (mass_cart + mass_pole)))
-    return top / bottom
+def getForce(force, length, thetaVelocity, theta, massCart, massPole):
+    tMass = massCart + massPole
+    return (force + massPole * length * thetaVelocity ** 2 * math.sin(theta)) / tMass
 
 
-def x_acc(length, force, mass_pole, mass_cart, theta, theta_dot, theta_acc):
-    return (force + mass_pole*length*((theta_dot**2)*math.sin(theta) - theta_acc*math.cos(theta))) / (mass_cart + mass_pole)
+def getThetaAccel(theta, forceVar, massPole, length, massCart):
+    tMass = massCart + massPole
+    return (9.8 * math.sin(theta) - math.cos(theta) * forceVar) / (
+            length * (4 / 3 - massPole * math.cos(theta) ** 2 / tMass))
 
 
+def getXAccel(forceVar, massPole, length, theta, thetaAccelleration, massCart):
+    return forceVar - massPole * length * thetaAccelleration * math.cos(theta) / (massCart + massPole)
 
 
-mass_pole = 0.2
-mass_cart = 4.0
+massPole = 0.2
+massCart = 4.0
 length = 1.0
 max_force = 6.0
-force = 0.0
+force = 10.0
 tau = 0.01
 theta = math.pi / 24
-theta_dot = 0
+thetaVelocity = 0
 last_theta = theta
 x = 0
-x_dot = 0
+xVelocity = 0
 t = 5
 
-#thetaAcc = thetaDoubleDot = accelleration
-#thetaDot = thetaDot = velocity
-#tau = DT = time interval
+# thetaAcc = thetaDoubleDot = acceleration
+# thetaDot = thetaDot = velocity
+# tau = DT = time interval
 
 while True:
-    curr_theta_acc = theta_acc(length, max_force, mass_pole, mass_cart, theta, theta_dot)
-    curr_x_acc = x_acc(length, max_force, mass_pole, mass_cart, theta, theta_dot, curr_theta_acc)
+    forceVar = getForce(force, length, thetaVelocity, theta, massCart, massPole)
+    thetaAcceleration = getThetaAccel(theta, forceVar, massPole, length, massCart)
+    xAcceleration = getXAccel(forceVar, massPole, length, theta, thetaAcceleration, massCart)
+    if xAcceleration > max_force / (massCart + massPole):
+        xAcceleration = max_force / (massCart + massPole)
 
-    theta_dot += curr_theta_acc * tau
-    last_theta = theta
-    theta += theta_dot * tau
-    x_dot += curr_x_acc
-    x += x_dot * tau
+    thetaOld = theta
+    theta += thetaVelocity * tau
+    thetaVelocity += thetaAcceleration * tau
+    x += xVelocity * tau
+    xVelocity += xAcceleration * tau
 
-    #comparing the new angle to the old angle
+    # comparing the new angle to the old angle
     if last_theta > theta:
-        #the angle decreased, increasing base angle by the amount of change
-        print("Pushed Weight to middle from " + str(last_theta) + " to " + str(theta) + "("+str(last_theta-theta)+")" )
-        theta = last_theta + (last_theta-theta)
+        # the angle decreased, increasing base angle by the amount of change
+        print("Pushed Weight to middle from " + str(last_theta) + " to " + str(theta) + "(" + str(
+            last_theta - theta) + ")")
+        theta = last_theta + (last_theta - theta)
+        thetaVelocity = 0
+        xVelocity = 0
+        x = 0
+
+
     else:
-        #the angle decreased, pole still dropping at max force
-        print("theta increased by " + str(theta-last_theta) + " test failed. Max recoverable angle is " +
+        # the angle decreased, pole still dropping at max force
+        print("theta increased by " + str(theta - last_theta) + " test failed. Max recoverable angle is " +
               str(last_theta))
         break
 print("Exited while loop")
 print("unrecoverable angle = " + str(last_theta))
-print("Unrecoverable angle (degrees) = " + str(last_theta*180/math.pi ))
-
+print("Unrecoverable angle (degrees) = " + str(last_theta * 180 / math.pi))
 
 # while t > 0 and math.pi / 2 > theta > -1 * math.pi / 2:
 #     #calculate the accellerations for theta and x

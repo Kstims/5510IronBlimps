47516d4b4f6362aba1ee4bdf42c8a976903c5b93
Josh Williams <joshua.williams@aggiemail.usu.edu>
2022-10-21 15:03:20 -0600

    Added Q5 learning model

--- Midterm/Q5c.py
+++ Midterm/Q5c.py
@@ -1,13 +1,32 @@
 # !pip install gym[classic_control]
 import gym
+import pickle
+import numpy as np
+
+# Use saved model.
+file = open('model.obj', 'rb')
+model = pickle.load(file)
+file.close()
+q_table = model["q_table"]
+bins = model["bins"]
 
 env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset(seed=42)
+observation, info = env.reset()
 
+
+# Transfer the continuous observation into the nearest matching discrete bin.
+def Discrete(state, bins):
+    index = []
+    for i in range(len(state)):
+        index.append(np.digitize(state[i], bins[i]) - 1)
+    return tuple(index)
+
+
+# Run an example of using cartpole model for 1000 steps.
 for _ in range(1000):
-    action = env.action_space.sample()
+    current_state = Discrete(env.reset()[0], bins)
+    action = np.argmax(q_table[current_state])
     observation, reward, terminated, truncated, info = env.step(action)
-
     env.render()
 
     if terminated or truncated:

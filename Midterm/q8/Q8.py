3159f25ca84bd909b5aeb69675821c02f032f53f
Kyle Stimson <stimsonkyle@hotmail.com>
2022-10-25 23:57:11 -0600

    Finished 8a

--- Midterm/q8/Q8.py
+++ Midterm/q8/Q8.py
@@ -1,19 +1,156 @@
-import tensorflow as tf
-from keras_preprocessing.image import load_img
-from keras_preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import decode_predictions
-from keras.applications.vgg16 import VGG16
-import numpy as np
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt # for plotting
+    import numpy as np # for transformation
+
+    import torch # PyTorch package
+    import torchvision # load datasets
+    import torchvision.transforms as transforms # transform data
+    import torch.nn as nn # basic building block for neural neteorks
+    import torch.nn.functional as F # import convolution functions like Relu
+    import torch.optim as optim # optimzer
+    import time
+
+    transform = transforms.Compose( # composing several transforms together
+        [transforms.ToTensor(), # to tensor object
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
+
+    # set batch_size
+    batch_size = 4
+
+    # set number of workers
+    num_workers = 2
+
+    # load train data
+    trainset = torchvision.datasets.ImageFolder(root='./train', transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    # load test data
+    testset = torchvision.datasets.ImageFolder(root='./test', transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    # put classes into a set
+    classes = ('crop', 'weed')
 
 
-model = tf.keras.models.load_model('model_saved.h5')
+    def imshow(img):
+      ''' function to show image '''
+      img = img / 2 + 0.5 # unnormalize
+      npimg = img.numpy() # convert to numpy objects
+      plt.imshow(np.transpose(npimg, (1, 2, 0)))
+      plt.show()
+
+    class Net(nn.Module):
+        ''' Models a simple Convolutional Neural Network'''
+
+        def __init__(self):
+            ''' initialize the network '''
+            super(Net, self).__init__()
+            # 3 input image channel, 6 output channels,
+            # 5x5 square convolution kernel
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            # Max pooling over a (2, 2) window
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 125 * 125, 120)  # 5x5 from image dimension     breaks
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 2)
+
+        def forward(self, x):
+            ''' the forward propagation algorithm '''
+            #print('x_shape:', x.shape)
+            x = self.pool(F.relu(self.conv1(x)))
+            #print('x_shape:', x.shape)
+            x = self.pool(F.relu(self.conv2(x)))
+            #print('x_shape:', x.shape)
+            x = x.view(-1, 16 * 125 * 125)
+            #print('x_shape:', x.shape)
+            x = F.relu(self.fc1(x)) #breaks here
+            #print('x_shape:', x.shape)
+            x = F.relu(self.fc2(x))
+            #print('x_shape:', x.shape)
+            x = self.fc3(x)
+            #print('x_shape:', x.shape)
+            return x
 
 
-for file in 'test':
-    image = load_img(file, target_size=(512, 512))
-    img = np.array(image)
-    img = img / 512.0
-    img = img.reshape(1, 511, 511, 3)
-    label = model.predict(img)
-    print("Predicted Class (0 - Crops , 1- Weeds): ", label[0][0])
+    net = Net()
+    print(net)
+
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    start.record()
+    printHeader = False
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
+            if i % 64 == 63:
+                if not printHeader:
+                    printHeader = True
+                    print("[Epoch, Idx]")
+                # print every 64 mini-batches
+                print('[%d,   %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 64))
+                running_loss = 0.0
+
+    # whatever you are timing goes here
+    end.record()
+
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
+
+    print('Finished Training')
+    #print(start.elapsed_time(end))  # milliseconds
+
+    # save
+    PATH = './CropWeedModel.pth'
+    torch.save(net.state_dict(), PATH)
+    # reload
+    #net = Net()
+    #net.load_state_dict(torch.load(PATH))
+
+
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # print images
+
+    print('True result: ', ' '.join('%s' % classes[labels[j]] for j in range(4)))
+
+    outputs = net(images)
+
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%s' % classes[predicted[j]] for j in range(4)))
+    imshow(torchvision.utils.make_grid(images))
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10 test images: %d %%' % (100 * correct / total))

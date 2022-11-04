e1887f4b449f34eb7722973c4389ee714e867a12
Kyle Stimson <stimsonkyle@hotmail.com>
2022-11-04 16:48:31 -0600

    Updated comments to make code more readable

--- Project/faceTrack.py
+++ Project/faceTrack.py
@@ -3,6 +3,7 @@ import cv2
 import faceTrackUtil as ftu
 import time
 
+#Makes connection between drone and computer, activates camera, and lifts off
 myDrone = ftu.intializeTello()
 print("Battery level: " + str(myDrone.get_battery()))
 myDrone.streamon()
@@ -16,17 +17,26 @@ w, h = 480, 360
 pid = [0.4, 0.4, 0]
 pError = 0
 
+#Main loop
 while True:
     # _, img = cap.read()
+    #receives image from camera feed
     img = myDrone.get_frame_read().frame
+    #resizes image to set width/height
     img = cv2.resize(img, (w, h))
+    #detects face in frame
     img, info = ftu.findFace(img)
+    #tracts face coordinates in frame to determine movement
     pError = ftu.trackFace(myDrone,info,w, h, pid,pError)
     # print("Center", info[0], "Area", info[1])
+    #displays image
     cv2.imshow("Output", img)
+
+    #press 'q' to land drone and shutdown
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         myDrone.streamoff()
         myDrone.land()
         myDrone.end()
+        print("Drone disengaged")
         break
--- Project/faceTrackUtil.py
+++ Project/faceTrackUtil.py
@@ -5,33 +5,33 @@ import pygame
 
 
 def intializeTello():
-    # CONNECT TO TELLO
+    #Connect to drone
     myDrone = Tello()
     myDrone.connect()
+    #initialize movement to zero
     myDrone.for_back_velocity = 0
     myDrone.left_right_velocity = 0
     myDrone.up_down_velocity = 0
     myDrone.yaw_velocity = 0
     myDrone.speed =0
-    #print(myDrone.get_battery())
-    #myDrone.streamoff()
-    #myDrone.streamon()
     return myDrone
 
-def telloGetFrame(myDrone,w=360,h=240):
-    # GET THE IMGAE FROM TELLO
+def telloGetFrame(myDrone,w,h):
+    # Get image from drone and resizes. Handled in main function
     myFrame = myDrone.get_frame_read()
     myFrame = myFrame.frame
     img = cv2.resize(myFrame, (w, h))
     return img
 
 def findFace(img):
-    # faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+    # Loads haar cascade from cv2 to detect front of face
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.2, 8)
+    #stores current faces into arrays
     myFaceListC = []
     myFaceListArea = []
+    #generates rectangle and center dot around each detected face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cx = x + w // 2
@@ -41,47 +41,38 @@ def findFace(img):
         myFaceListC.append([cx, cy])
         myFaceListArea.append(area)
 
+    #returns the face for tracking
     if len(myFaceListArea) != 0:
         i = myFaceListArea.index(max(myFaceListArea))
         return img, [myFaceListC[i], myFaceListArea[i]]
     else:
         return img, [[0, 0], 0]
 
-# def trackFace(myDrone,c,w,pid,pError):
-#     print(c)
-#     error = c[0][0] - w//2
-#     # Current Value - Target Value
-#     speed = int(pid[0]*error + pid[1] * (error-pError))
-#     if c[0][0] != 0:
-#         myDrone.yaw_velocity = speed
-#     else:
-#         myDrone.left_right_velocity = 0
-#         myDrone.for_back_velocity = 0
-#         myDrone.up_down_velocity = 0
-#         myDrone.yaw_velocity = 0
-#         error = 0
-# # SEND VELOCITY VALUES TO TELLO
-#     if myDrone.send_rc_control:
-#         myDrone.send_rc_control(myDrone.left_right_velocity, myDrone.for_back_velocity,
-#         myDrone.up_down_velocity, myDrone.yaw_velocity)
-#     return error
-
+#takes the drone connect data, the face info, frame width and height. PID, and error range
 def trackFace(myDrone, info, w, h, pid, pError):
     print(info)
+    #range of distance to control drone moving forward/away from target
     fbRange = [3200, 6800]
+    #area of the face square
     area = info[1]
+    #x/y pos of the face square
     x, y = info[0]
-
+    #set flag to print out target x/y pos and velocities
+    debugOutput = False
     fb = 0
+    #offset from the center
     error = x - w // 2
     errorHeight = y - h //2
 
+    #gets the required rotation speed to keep drone centered on target
     rotationSpeed = pid[0] * error + pid[1] * (error - pError)
     rotationSpeed = int(np.clip(rotationSpeed, -100, 100))
+    #gets required vertical speed to keep drone centered on target
     heightSpeed = pid[0] * errorHeight + pid[1] * (errorHeight - pError)
     heightSpeed = -1* int(np.clip(heightSpeed, -20, 20))
 
 
+    #determines if target is too far or close to the drone. Moves closer/away if needed.
     if area > fbRange[0] and area < fbRange[1]:
         fb = 0
     elif area > fbRange[1]:
@@ -94,10 +85,12 @@ def trackFace(myDrone, info, w, h, pid, pError):
         error = 0
     # print(speed, fb)
     #myDrone.send_rc_control(0, fb, 0, rotationSpeed)
-    print("x: " + str(x))
-    print("y: " + str(y))
-    print("errorHeight: " + str(errorHeight))
-    print("heightSpeed: " + str(heightSpeed))
+    if debugOutput:
+        print("x: " + str(x))
+        print("y: " + str(y))
+        print("errorHeight: " + str(errorHeight))
+        print("heightSpeed: " + str(heightSpeed))
+    #sends movement commands to keep target cenetered
     myDrone.send_rc_control(0, fb, heightSpeed, rotationSpeed)
     return error
 
